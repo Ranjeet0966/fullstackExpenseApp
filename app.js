70e76a7e6f8f337010d404a6761df5c1a583832b
Ranjeet Kumar Yadav <innovativeranjeet@gmail.com>
2023-09-04 22:17:28 -0400

    fullstackexpense-tracker

---
+++ app.js
@@ -0,0 +1,25 @@
+const path=require('path');
+const express=require('express');
+const bodyparser=require('body-parser');
+const sequelize=require('./util/database');
+const User=require('./models/User')
+const userRoutes=require('./routes/user')
+var cors=require('cors')
+const app=express();
+app.use(bodyparser.json({extended:false}))
+app.use(cors());
+app.use('/user',userRoutes)
+
+app.use((req, res)=>{
+  res.sendFile(path.join(__dirname,req.url));
+})
+
+  sequelize
+  .sync()
+  .then(result => {
+    console.log('result');
+    app.listen(3500);
+  })
+  .catch(err => {
+    console.log(err);
+  });
